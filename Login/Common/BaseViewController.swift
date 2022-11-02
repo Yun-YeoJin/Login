ef8e342a7f54aa12b575ed1719b0507e104bbddf
Yun <duwls0349@naver.com>
2022-11-02 17:05:40 +0900

    Feat: BaseViewController 생성

---
+++ Login/Common/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  Login
+//
+//  Created by 윤여진 on 2022/11/02.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        setConstraints()
+        
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+    
+}
+
