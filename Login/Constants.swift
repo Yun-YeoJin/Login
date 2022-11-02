82c0844a26a502fd69776bb702539238a92b194f
Yun <duwls0349@naver.com>
2022-11-02 19:35:28 +0900

    Feat: Color와 Design Constants 생성

---
+++ Login/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Login
+//
+//  Created by 윤여진 on 2022/11/02.
+//
+
+import UIKit
+
+enum Constants {
+    
+    enum Design {
+        static let cornerRadius: CGFloat = 10
+        static let borderWidth: CGFloat = 1
+    }
+}
+
+extension Constants {
+    
+    enum BaseColor {
+        static let background = UIColor.systemBackground
+        static let border = UIColor.label.cgColor
+        static let text = UIColor.label
+        static let placeholder = UIColor.systemMint
+        static let textFieldBackground = UIColor.systemGray5
+    }
+    
+}
