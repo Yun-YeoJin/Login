390c080b5e9e62d6c2da81ce5439c83367f39eeb
Yun <duwls0349@naver.com>
2022-11-04 01:48:48 +0900

    refactor: APIService 코드 간소화

--- Login/APIService.swift
+++ Login/APIService.swift
@@ -11,107 +11,34 @@ import UIKit
 import RxCocoa
 import RxSwift
 
-struct Login: Codable {
-    let token: String
-}
-
-struct User: Codable {
-    let photo: String
-    let email: String
-    let username: String
-}
-
-struct Profile: Codable {
-    let user: User
-}
-
 class APIService {
     
-    func signup(username: String, email: String, password: String) {
-        let api = SeSACAPI.signup(userName: username, email: email, password: password)
-        
-        AF.request(api.url, method: .post, parameters: api.parameters, headers: api.header).responseString { response in
-            
-            print(response)
-            print(response.response?.statusCode)
-            
-        }
-        
-    }
+    static let shared = APIService()
     
-    func login(email: String, password: String) {
-        let api = SeSACAPI.login(email: email, password: password)
+    private init() { }
+    
+    func requestSeSAC<T: Decodable>(type: T.Type = T.self, url: URL, method: HTTPMethod, parameters: [String:String]? = nil, headers: HTTPHeaders, completion: @escaping (Result<T, Error>) -> Void) {
         
-        AF.request(api.url, method: .post, parameters: api.parameters, headers: api.header)
-            .validate(statusCode: 200...299)
-            .responseDecodable(of: Login.self) { response in
+        AF.request(url, method: .get, parameters: parameters, headers: headers)
+            .responseDecodable(of: type) { response in
                 
                 switch response.result {
                     
                 case .success(let data): //200번대만 성공
-                    print(data.token)
-                    UserDefaults.standard.set(data.token, forKey: "token")
+                    completion(.success(data)) //탈출 클로저, enum 등
                     
-                case .failure(_): //나머지는 실패
-                    print(response.response?.statusCode)
-                }
-            }
-    }
-    
-    func profile() {
-        
-        let url = SeSACAPI.profile.url
-        let header: HTTPHeaders = SeSACAPI.profile.header
-        
-        AF.request(url, method: .get, headers: header)
-            .responseDecodable(of: Profile.self) { response in
-                
-                switch response.result {
+                case .failure(let error): //나머지는 실패
                     
-                case .success(let data): //200번대만 성공
-                    print(data)
+                    guard let statusCode = response.response?.statusCode else { return }
+                    guard let error = SeSACError(rawValue: statusCode) else { return }
                     
-                case .failure(_): //나머지는 실패
-                    print(response.response?.statusCode)
+                    completion(.failure(error))
                 }
                 
             }
     }
-    
 }
 
-//class SeSACAPIManager {
-//
-//    static func requestSeSAC(completion: @escaping (Profile?) -> Void ) {
-//
-//        let sourceURL = SeSACAPI.profile.url
-//
-//        var request = URLRequest(url: sourceURL)
-//
-//        request.setValue("\(SeSACAPI.profile.header)", forHTTPHeaderField: "Authorization")
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//
-//            if let _ = error {
-//                print(error)
-//                return
-//            }
-//
-//            guard let response = response as? HTTPURLResponse,(200...299).contains(response.statusCode) else { return }
-//
-//            if let data = data, let profileData = try?
-//                JSONDecoder().decode(Profile.self, from: data) {
-//                completion(profileData)
-//                return
-//            }
-//            completion(nil)
-//
-//        }.resume()
-//
-//    }
-//
-//
-//}
 
 
 
