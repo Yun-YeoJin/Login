cd92bfc4b616ffe4658cd51cdac2f6f28bd5e6d0
Yun <duwls0349@naver.com>
2022-11-04 02:09:47 +0900

    Feat: 회원가입 ViewModel 생성

--- Login/ViewModel/SignupViewModel.swift
+++ Login/ViewModel/SignupViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+import Alamofire
 import RxCocoa
 import RxSwift
 
@@ -18,17 +19,58 @@ protocol CommonViewModel {
     func transform(input: Input) -> Output
 }
 
+final class SignupViewModel2 {
+        
+    var nickName = BehaviorRelay<String>(value: "닉네임을 입력해주세요")
+    var email = BehaviorRelay<String>(value: "올바른 이메일을 입력해주세요")
+    var password = BehaviorRelay<String>(value: "8자리 이상 비밀번호를 입력해주세요")
+    var isOK = BehaviorSubject<Bool>(value: false)
+    
+    var isValid: Observable<Bool> {
+        return Observable
+            .combineLatest(email, password)
+            .map { email, password in
+                print(email, password)
+                return email.count > 0 && email.contains("@") && email.contains(".") && password.count >= 8
+            }
+    }
+    
+        
+    func requestSignup() {
+        let api = SeSACAPI.signup(userName: nickName.value,
+                                  email: email.value,
+                                  password: password.value)
+        
+        AF.request(api.url, method: .post, parameters: api.parameters, headers: api.header).responseString { [weak self] response in
+            guard let self = self else { return }
+
+            switch response.result {
+            case .success:
+
+                self.isOK.onNext(true)
+
+            case .failure:
+                print("error")
+            }
+        }
+    }
+}
+
+//TODO: INPUT OUTPUT을 사용해서 하려고 했으나 실패..
 final class SignupViewModel: CommonViewModel {
-  
-    let nickNameValidText = BehaviorRelay(value: "닉네임은 3자 이상 입력해주세요.")
-    let emailValidText = BehaviorRelay(value: "올바른 이메일을 입력해주세요.")
-    let passwordValidText = BehaviorRelay(value: "비밀번호는 최소 6자 이상 입력해주세요.")
+    
+    var nickName = BehaviorRelay<String>(value: "닉네임을 입력해주세요")
+    var email = BehaviorRelay<String>(value: "이메일을 입력해주세요")
+    var password = BehaviorRelay<String>(value: "비밀번호를 입력해주세요")
+    
+    var isOK = BehaviorSubject<Bool>(value: false)
     
     struct Input {
         let tap: ControlEvent<Void>
         let nickNameText: ControlProperty<String?>
         let emailText: ControlProperty<String?>
         let passwordText: ControlProperty<String?>
+        
     }
     
     struct Output {
@@ -45,7 +87,7 @@ final class SignupViewModel: CommonViewModel {
         
         let nickNameValid = input.nickNameText
             .orEmpty
-            .map { $0.count < 3 }
+            .map { $0.count < 4 }
             .share()
         
         let emailValid = input.emailText
@@ -55,17 +97,46 @@ final class SignupViewModel: CommonViewModel {
         
         let passwordValid = input.passwordText
             .orEmpty
-            .map { $0.count < 6 }
+            .map { $0.count < 8 }
             .share()
-    
         
-        let nickNameText = nickNameValidText.asDriver()
-        let emailText = emailValidText.asDriver()
-        let passwordText = passwordValidText.asDriver()
+        
+        let nickNameText = nickName.asDriver()
+        let emailText = email.asDriver()
+        let passwordText = password.asDriver()
         
         return Output(tap: input.tap, nickNameText: nickNameText, emailText: emailText, passwordText: passwordText, nickNameValid: nickNameValid, emailValid: emailValid, passwordValid: passwordValid)
         
     }
     
-    
+    func requestSignup() {
+        
+        let api = SeSACAPI.signup(userName: nickName.value, email: email.value, password: password.value)
+        
+        AF.request(api.url, method: .post, parameters: api.parameters, headers: api.header).responseString { [weak self] response in
+            guard let self = self else { return }
+            
+            switch response.result {
+            case .success:
+                self.isOK.onNext(true)
+                
+            case .failure:
+                print("error")
+            }
+        }
+        
+        //        APIService.shared.requestSeSAC(type: Login.self, url: api.url, method: .post, parameters: api.parameters, headers: api.header) { [weak self] response in
+        //
+        //            switch response {
+        //            case .success(_):
+        //                self?.isOK.onNext(true)
+        //
+        //            case .failure(let error):
+        //                self?.isOK.onError(error)
+        //            }
+        //
+        //        }
+        
+    }
 }
+
