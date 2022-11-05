9e29bcd68e3c198aa965aa429861077fdaec5fd2
Yun <duwls0349@naver.com>
2022-11-06 01:13:08 +0900

    Feat: 로그인 화면 Rx와 MVVM을 이용한 ViewController 생성

--- Login/LoginScene/View/LoginViewController.swift
+++ Login/LoginScene/View/LoginViewController.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 final class LoginViewController: BaseViewController {
     
     let mainView = LoginView()
+    
+    let viewModel = LoginViewModel()
+    
+    var disposeBag = DisposeBag()
     
     override func loadView() {
         self.view = mainView
@@ -23,6 +30,11 @@ final class LoginViewController: BaseViewController {
         
         mainView.signupButton.addTarget(self, action: #selector(signupButtonClicked), for: .touchUpInside)
         
+        view.backgroundColor = .systemBackground
+        
+        bindData()
+        
+        
     }
     
     @objc func signupButtonClicked() {
@@ -31,5 +43,65 @@ final class LoginViewController: BaseViewController {
         transition(vc, transitionStyle: .push)
         
     }
+    
+    func bindData() {
+        
+        viewModel.email
+            .asDriver()
+            .drive(mainView.emailTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        viewModel.password
+            .asDriver()
+            .drive(mainView.passwordTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        mainView.emailTextField.rx.text.orEmpty
+            .asDriver()
+            .drive(onNext: { [weak self] text in
+                self?.viewModel.email.accept(text)
+            })
+            .disposed(by: disposeBag)
+        
+        mainView.passwordTextField.rx.text.orEmpty
+            .asDriver()
+            .drive(onNext: { [weak self] text in
+                self?.viewModel.password.accept(text)
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.isValid
+            .asDriver(onErrorJustReturn: false)
+            .drive(mainView.loginButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        viewModel.isValid
+            .map { $0 == true ? UIColor.systemMint : UIColor.opaqueSeparator }
+            .asDriver(onErrorJustReturn: .opaqueSeparator)
+            .drive(mainView.loginButton.rx.backgroundColor)
+            .disposed(by: disposeBag)
+        
+        mainView.loginButton.rx.tap
+            .withUnretained(self)
+            .bind { vc, _ in
+                vc.viewModel.requestLogin()
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.isOK
+            .withUnretained(self)
+            .observe(on: MainScheduler.instance)
+            .subscribe { vc, value in
+                if value {
+                    vc.navigationController?.pushViewController(ProfileViewController(), animated: true)
+                    vc.showSesacAlert(title: "로그인에 성공했습니다.", message: "", buttonTitle: "확인")
+                }
+                
+            } onError: { error in
+                self.showSesacAlert(title: "로그인에 실패했습니다.", message: "다시 확인해보세요", buttonTitle: "확인")
+            }
+            .disposed(by: disposeBag)
+    }
+    
     
 }
